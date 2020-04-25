7ccc15ec220afcda476c6eea6a1b713c1cecfc8a
Colin Sagan <careof@colinsagan.com>
2020-04-25 13:36:30 +0800

    Connect form with api app.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import React from 'react';
 import './App.css';
+import feathers from '@feathersjs/feathers';
+import io from 'socket.io-client';
+import React from 'react';
+import socketio from '@feathersjs/socketio-client';
 
 function App() {
+  // Feathers api setup
+  const baseUrl = {
+    rest: 'http://appserver.socialbuyingapi.internal:3030',
+    socket: 'http://social-buying-api.lndo.site',
+  };
+
+  // REST
+  // const rest = require('@feathersjs/rest-client');
+  // const client = feathers();
+
+  // // Connect to a different URL
+  // const restClient = rest(baseUrl.rest);
+
+  // // Configure an AJAX library (see below) with that client
+  // client.configure(restClient.fetch(window.fetch));
+
+  // // Connect to the `http://feathers-api.com/invites` service
+  // const inviteService = client.service('invites');
+
+  // Websockets
+  const socket = io(baseUrl.socket, {
+    transports: ['websocket'],
+    forceNew: true
+  });
+
+  const client = feathers();
+  client.configure(socketio(socket));
+
+  const inviteService = client.service('invites');
+
+  inviteService.on('created', invite => console.log('Created a invite', invite));
 
   function submitRequest(event) {
     event.preventDefault();
-    // @TODO: Feathers client
+
+    // Use the invites service from the server
+    inviteService.create({
+      text: event.target.request.value
+    }).then((data) => {
+      console.log(data);
+    });
+
+
   }
 
   return (
@@ -13,8 +55,20 @@ function App() {
       <header>
         <h1>Social Buying</h1>
       </header>
-      <form className="submit-request" onSubmit={submitRequest}>
-        <input type="text" />
+      <form
+        className="submit-request"
+        onSubmit={submitRequest}
+      >
+        <input
+          type="text"
+          name="request"
+        />
+        <button
+          type="submit"
+          className="request-submit"
+        >
+          Request
+        </button>
       </form>
     </div>
   );
