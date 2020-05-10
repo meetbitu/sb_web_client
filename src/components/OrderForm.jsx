05b613aeda08ef7ffec955520c0948f96862925c
Colin Sagan <careof@colinsagan.com>
2020-05-10 21:51:34 +0800

    Create a nicer copy invite link experience.

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -3,6 +3,7 @@ import React, {
   useState,
   useEffect,
 } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const OrderForm = ({ invite, orderService }) => {
   const initialInput = {
@@ -12,6 +13,9 @@ const OrderForm = ({ invite, orderService }) => {
   };
   const [input, setInput] = useState(initialInput);
   const [message, setMessage] = useState();
+
+  const defaultCopyText = 'Copy invite link to send to your friends';
+  const [copiedText, setCopiedText] = useState(defaultCopyText);
 
   // Keep the name assuming the same person is ordering again
   const resetInput = {
@@ -36,6 +40,8 @@ const OrderForm = ({ invite, orderService }) => {
   function submitRequest(event) {
     event.preventDefault();
 
+    // @TODO: Validate required fields on form
+
     // Use the orders service from the server
     orderService.create({
       name: input.name,
@@ -49,69 +55,75 @@ const OrderForm = ({ invite, orderService }) => {
 
   }
 
+  function inviteEmoji() {
+    return (copiedText === defaultCopyText) ?
+      { __html: '🤝' } :
+      { __html: '✨🍛✨'};
+  }
+
   return (
-    <form
-      className="submit-order"
-      onSubmit={submitRequest}
-    >
+    <div className="order-form">
       <header>
         <h2>{invite.text}</h2>
-        <label>
-          Copy this link and send to your friends
-          <input
-            type="text"
-            name="share-link"
-            className="copy-share-link"
-            value={encodeURI(`${window.location.protocol}${window.location.host}?invite=${invite._id}&text=${invite.text}`)}
-            onChange={() => {}}
-          />
-        </label>
+        <CopyToClipboard text={encodeURI(`${window.location.protocol}${window.location.host}?invite=${invite._id}&text=${invite.text}`)}
+          onCopy={() => setCopiedText('Invite link has been copied to your clipboard')}>
+          <button>{copiedText}</button>
+        </CopyToClipboard>
+        <div
+          className="invite-copy-emoji"
+          dangerouslySetInnerHTML={inviteEmoji()}
+        />
       </header>
-      <input
-        type="text"
-        name="name"
-        value={input.name}
-        onChange={handleInputChange}
-        placeholder="Name"
-      />
-      <select
-        name="order"
-        onChange={handleInputChange}
-        value={input.order}
+      <form
+        className="submit-order"
+        onSubmit={submitRequest}
       >
-        <option value="">Select dish</option>
-        <option value="Chicken Katsu">Chicken Katsu</option>
-        <option value="Port Katsu">Pork Katsu</option>
-      </select>
-      <select
-        name="spice"
-        onChange={handleInputChange}
-        value={input.spice}
-      >
-        <option value="">Select spice level</option>
-        <option value="Level 1">Level 1</option>
-        <option value="Level 2">Level 2</option>
-        <option value="Level 3">Level 3</option>
-        <option value="Level 4">Level 4</option>
-        <option value="Level 5">Level 5</option>
-        <option value="Level 6">Level 6</option>
-        <option value="Level 7">Level 7</option>
-        <option value="Level 8">Level 8</option>
-        <option value="Level 9">Level 9</option>
-        <option value="Level 10">Level 10</option>
-      </select>
-      <div className="form-actions">
-        <button
-          type="submit"
-          className="order-submit"
+        <input
+          type="text"
+          name="name"
+          value={input.name}
+          onChange={handleInputChange}
+          placeholder="Name"
+        />
+        <select
+          name="order"
+          onChange={handleInputChange}
+          value={input.order}
         >
-          Thankssss!
-        </button>
-      </div>
-      <footer>
-        <h3 className="messages">{message}</h3>
-      </footer>
-    </form>
+          <option value="">Select dish</option>
+          <option value="Chicken Katsu">Chicken Katsu</option>
+          <option value="Port Katsu">Pork Katsu</option>
+        </select>
+        <select
+          name="spice"
+          onChange={handleInputChange}
+          value={input.spice}
+        >
+          <option value="">Select spice level</option>
+          <option value="Level 1">Level 1</option>
+          <option value="Level 2">Level 2</option>
+          <option value="Level 3">Level 3</option>
+          <option value="Level 4">Level 4</option>
+          <option value="Level 5">Level 5</option>
+          <option value="Level 6">Level 6</option>
+          <option value="Level 7">Level 7</option>
+          <option value="Level 8">Level 8</option>
+          <option value="Level 9">Level 9</option>
+          <option value="Level 10">Level 10</option>
+        </select>
+        <div className="form-actions">
+          <button
+            type="submit"
+            className="order-submit"
+          >
+            Thankssss!
+          </button>
+        </div>
+        <footer>
+          <h3 className="messages">{message}</h3>
+        </footer>
+      </form>
+    </div>
   );
 }
 
