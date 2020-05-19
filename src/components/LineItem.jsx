47829c0f87fe6e61bfa9b58e4dfce9206846628b
Colin Sagan <careof@colinsagan.com>
2020-05-19 10:31:05 +0800

    Move comment out of return statement of LineItem component it was breaking the build when minified.

--- src/components/LineItem.jsx
+++ src/components/LineItem.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function LineItem({ item }) {
+  // @TODO: the key should be a line item id once we start storing that in the db
   return (
-    // @TODO: the key should be a line item id once we start storing that in the db
     <div className="line-item" key={item.order}>
       {item.quantity}x {item.order} {item.options} <span className="price">₱{item.price}</span>
       {item.additions &&
